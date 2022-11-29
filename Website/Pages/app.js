fdbdd9df689b5948a740bbe9f92b5d3d32906700
rickycabra <rickycabra0@gmail.com>
2022-11-28 22:25:03 -0800

    first commit

---
+++ Website/Pages/app.js
@@ -0,0 +1,9 @@
+const q1 = "What most effective way to stay warm?";
+const a1 = ["Layering", "wear one jacket", "wear a long sleeve"];
+const q2 = "What is the most effective way to layer";
+const a2 = ["start with lighter layers and add heavier layers", "Start with the heaviest layer and add lighter ones"];
+const q3 = "It is raining outside and its cold how do you layer";
+const a3 = ["wear your raincot ontop of your cotton layers", "wear your raincoat underneath and a fleace over it", "wear a long sleeve t-shirt"];
+const q4 = "It snowed last night how do you dress";
+const a4 = ["wear warm clothing with snow boots", "wear warm clothing with vans", "wear warm clothin with flip flops"];
+
